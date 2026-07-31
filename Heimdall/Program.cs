cdba1056a0b7f993dee355a05f4ee826a9abd39e
Yams-01 <samysmailpro@gmail.com>
2026-07-31 14:40:16 +0200

    Print : PID --- ProcName --- StartTime

--- Heimdall/Program.cs
+++ Heimdall/Program.cs
@@ -1,1 +1,50 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System.ComponentModel;
+using System.Diagnostics;
+
+namespace Heimdall
+{
+    class Program
+    {
+
+        private static void WriteColor(string message, ConsoleColor clr)
+        {
+            Console.ForegroundColor = clr;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+
+
+        static void PrintProcess(Process processes)
+        {
+            try
+            {
+
+                Console.WriteLine($"{processes.Id} --- {processes.ProcessName} --- {processes.StartTime}");
+            }
+            catch (NotSupportedException e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+            catch (Win32Exception win )
+            {
+                WriteColor($"{win.Message} : {processes.ProcessName}", ConsoleColor.Red);
+            }
+
+        }
+         
+
+        static void Main()
+        {
+            Process[] allProccesses = Process.GetProcesses();
+            Process activeProc = Process.GetCurrentProcess();
+
+            foreach (Process process in allProccesses)
+            {
+                PrintProcess(process);
+                
+            }
+            
+        }
+    }
+}
