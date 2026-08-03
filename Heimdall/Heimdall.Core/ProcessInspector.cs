da218368823f1443d5e3134f36e8e1740f44b8ea
Yams-01 <samysmailpro@gmail.com>
2026-08-03 21:42:44 +0200

    follow up commit

---
+++ Heimdall/Heimdall.Core/ProcessInspector.cs
@@ -0,0 +1,40 @@
+﻿using System.Diagnostics;
+using System.ComponentModel;
+namespace Heimdall.Core;
+
+public static class ProcessInspector
+{
+    private static void WriteColor(string message, ConsoleColor clr)
+    {
+        Console.ForegroundColor = clr;
+        Console.WriteLine(message);
+        Console.ResetColor();
+    }
+
+
+    public static void PrintProcess(Process process)
+    {
+
+        try
+        {
+            string description = process.MainModule?.FileVersionInfo?.FileDescription;
+            string name = string.IsNullOrWhiteSpace(description) ? process.ProcessName : description; 
+                
+            Console.WriteLine($"{process.Id} --- {name} --- {process.StartTime}");
+        }
+        catch (NotSupportedException e)
+        {
+            WriteColor($"{e}", ConsoleColor.Blue);
+        }
+        catch (Win32Exception win)
+        {
+            WriteColor($"{win.Message} : {process.ProcessName}", ConsoleColor.Red);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+        }
+          
+
+    }
+}
