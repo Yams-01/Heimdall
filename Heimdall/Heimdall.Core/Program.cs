69514c603a981651d0acedb92cf56265e2b61b69
Yams-01 <samysmailpro@gmail.com>
2026-08-03 21:41:42 +0200

    Clean name printed, convetion based class seperation

---
+++ Heimdall/Heimdall.Core/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics;
+
+namespace Heimdall.Core;
+
+  
+    public class Program
+    {
+        static void Main()
+        {
+            var allProcesses = Process.GetProcesses();
+
+            foreach (var process in allProcesses)
+            {
+               ProcessInspector.PrintProcess(process);
+                
+            }
+            
+        }
+    }
